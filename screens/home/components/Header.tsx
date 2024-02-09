8d7c2fbde3752eaa1a4dabf33d89ee580366475f
obeim <obei.m2017@gmail.com>
2024-02-09 22:01:03 +0200

    feat: change theme icon animation

--- screens/home/components/Header.tsx
+++ screens/home/components/Header.tsx
@@ -4,7 +4,7 @@ import Sun from "@/assets/icons/Sun.svg";
 import Menu from "@/assets/icons/Menu.svg";
 import { useColorScheme } from "nativewind";
 import { storage } from "@/utils";
-
+import { Motion } from "@legendapp/motion";
 export function Header() {
   const { colorScheme, toggleColorScheme } = useColorScheme();
 
@@ -26,18 +26,22 @@ export function Header() {
         className=" h-18 w-32 pr-4 flex-row items-center justify-end "
       >
         <View className="flex-row items-center justify-end p-2 rounded-full bg-lotion dark:bg-blackCoral">
-          <Text
+          <Motion.Text
+            animate={{ x: colorScheme === "light" ? 0 : -25 }}
+            transition={{ type: "spring" }}
             className={` text-primary dark:text-primaryDark/60 mx-2 mt-1 font-HelveticaRoman text-xs transition-transform transform duration-200 ${
               colorScheme === "light" ? "" : "-translate-x-6"
             }`}
           >
             {colorScheme === "dark" ? "فاتح" : "غامق"}
-          </Text>
-          {colorScheme === "dark" ? (
-            <Sun className={`my-auto translate-x-9 `} width={25} height={20} />
-          ) : (
-            <Moon className={`my-auto`} width={22} height={16} />
-          )}
+          </Motion.Text>
+          <Motion.View animate={{ x: colorScheme === "light" ? 0 : 32 }}>
+            {colorScheme === "dark" ? (
+              <Sun width={25} height={20} />
+            ) : (
+              <Moon width={22} height={16} />
+            )}
+          </Motion.View>
         </View>
       </Pressable>
     </View>
