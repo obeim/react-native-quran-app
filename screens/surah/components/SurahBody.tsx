65be40d5acdf4efe6d2c724ada02b58085c82af1
Obai Mustafa <56155793+obeim@users.noreply.github.com>
2025-10-11 15:54:08 +0300

    refactor: surah page

---
+++ screens/surah/components/SurahBody.tsx
@@ -0,0 +1,93 @@
+import { useAudioPlayer } from "expo-audio";
+import { memo } from "react";
+import { View } from "react-native";
+import { useState, useCallback, useEffect } from "react";
+
+import { storage } from "@/utils";
+import Fav from "@/utils/Favs";
+import { Ayah, FavType, SurahwithAyat } from "@/types";
+
+import { PageView } from "../components/PageView";
+import { AyatView } from "../components/AyatView";
+
+import { AyahActionsWrapper } from "../components/AyahActionsWrapper";
+
+import { Header } from "../Header";
+
+const HeaderMemo = memo(Header);
+const PageViewMemo = memo(PageView);
+const AyatViewMemo = memo(AyatView);
+const AyahActionsWrapperMemo = memo(AyahActionsWrapper);
+
+const SurahBody = ({ data }: { data: SurahwithAyat }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedAyah, setSelectedAyah] = useState<Ayah>();
+  const [Favs, setFavs] = useState<FavType[]>([]);
+  const [openedModal, setOpenModal] = useState(false);
+  const [layout, setLayout] = useState<"page" | "ayat">(
+    (storage.getString("view_pref") as "page") || "ayat"
+  );
+
+  const player = useAudioPlayer();
+
+  useEffect(() => {
+    setFavs(Fav.getFav() || []);
+  }, []);
+
+  const onPressAyah = useCallback((aya: Ayah) => {
+    setSelectedAyah(aya);
+    setOpenModal(true);
+  }, []);
+
+  return (
+    <>
+      <AyahActionsWrapperMemo
+        Favs={Favs}
+        player={player}
+        close={() => setOpenModal(false)}
+        opened={openedModal}
+        ayah={selectedAyah}
+        currentPage={currentPage}
+      />
+      <HeaderMemo
+        player={player}
+        layout={layout}
+        setLayout={setLayout}
+        title={data.name_ar?.slice(5)}
+        subtitle={` ${data.ayat?.length} أيات - ${
+          { Meccan: "مكية", Medinan: "مدنية" }[
+            (data.type as "Meccan") || "Meccan"
+          ]
+        }`}
+      />
+
+      <View className="bg-white dark:bg-darkBg">
+        {
+          {
+            page: (
+              <View style={{ display: "flex" }}>
+                <PageViewMemo
+                  setCurrentPage={setCurrentPage}
+                  Favs={Favs}
+                  onPressAyah={onPressAyah}
+                  data={data}
+                />
+              </View>
+            ),
+            ayat: (
+              <View style={{ display: "flex" }}>
+                <AyatViewMemo
+                  Favs={Favs}
+                  onPressAyah={onPressAyah}
+                  data={data}
+                />
+              </View>
+            ),
+          }[layout]
+        }
+      </View>
+    </>
+  );
+};
+
+export default SurahBody;
--- screens/surah/index.tsx
+++ screens/surah/index.tsx
@@ -1,21 +1,10 @@
 import { useLocalSearchParams } from "expo-router";
-import { Header } from "./Header";
 import { View } from "react-native";
-import React, { useState, useMemo, useCallback, useEffect } from "react";
-import { PageView } from "./components/PageView";
-import { AyatView } from "./components/AyatView";
-import { storage } from "@/utils";
-import { Ayah, FavType, SurahwithAyat } from "@/types";
-import { AyahActionsWrapper } from "./components/AyahActionsWrapper";
-import Fav from "@/utils/Favs";
+import { useMemo } from "react";
+
 import { useKeepAwake } from "expo-keep-awake";
 import { useGetSurahWithAyat } from "@/utils/db/useGetSurahWithAyat";
-import { useAudioPlayer } from "expo-audio";
-
-const HeaderMemo = React.memo(Header);
-const PageViewMemo = React.memo(PageView);
-const AyatViewMemo = React.memo(AyatView);
-const AyahActionsWrapperMemo = React.memo(AyahActionsWrapper);
+import SurahBody from "./components/SurahBody";
 
 const Surah = () => {
   useKeepAwake();
@@ -28,7 +17,7 @@ const Surah = () => {
   const data = useGetSurahWithAyat(surahId);
 
   if (!data) return null;
-  console.log("MyComponent rendered"); // <-- logs on every render
+
   return (
     <View className="h-full">
       <SurahBody data={data} />
@@ -37,74 +26,3 @@ const Surah = () => {
 };
 
 export default Surah;
-
-const SurahBody = ({ data }: { data: SurahwithAyat }) => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [selectedAyah, setSelectedAyah] = useState<Ayah>();
-  const [Favs, setFavs] = useState<FavType[]>([]);
-  const [openedModal, setOpenModal] = useState(false);
-  const [layout, setLayout] = useState<"page" | "ayat">(
-    (storage.getString("view_pref") as "page") || "ayat"
-  );
-
-  const player = useAudioPlayer();
-
-  useEffect(() => {
-    setFavs(Fav.getFav() || []);
-  }, []);
-
-  const onPressAyah = useCallback((aya: Ayah) => {
-    setSelectedAyah(aya);
-    setOpenModal(true);
-  }, []);
-
-  return (
-    <>
-      <AyahActionsWrapperMemo
-        Favs={Favs}
-        player={player}
-        close={() => setOpenModal(false)}
-        opened={openedModal}
-        ayah={selectedAyah}
-        currentPage={currentPage}
-      />
-      <HeaderMemo
-        player={player}
-        layout={layout}
-        setLayout={setLayout}
-        title={data.name_ar?.slice(5)}
-        subtitle={` ${data.ayat?.length} أيات - ${
-          { Meccan: "مكية", Medinan: "مدنية" }[
-            (data.type as "Meccan") || "Meccan"
-          ]
-        }`}
-      />
-
-      <View className="bg-white dark:bg-darkBg">
-        {
-          {
-            page: (
-              <View style={{ display: "flex" }}>
-                <PageViewMemo
-                  setCurrentPage={setCurrentPage}
-                  Favs={Favs}
-                  onPressAyah={onPressAyah}
-                  data={data}
-                />
-              </View>
-            ),
-            ayat: (
-              <View style={{ display: "flex" }}>
-                <AyatViewMemo
-                  Favs={Favs}
-                  onPressAyah={onPressAyah}
-                  data={data}
-                />
-              </View>
-            ),
-          }[layout]
-        }
-      </View>
-    </>
-  );
-};
